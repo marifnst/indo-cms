f74d753d4e4830b3bd67c15ead0c12bd10834c0e
marifnst <arif.nasution@emeriocorp.com>
2019-08-11 19:17:50 +0700

    additional be to add, edit and delete

--- postgresql-service/src/main/java/com/indocms/postgresqlservice/controller/PostgresqlController.java
+++ postgresql-service/src/main/java/com/indocms/postgresqlservice/controller/PostgresqlController.java
@@ -9,7 +9,9 @@ import com.indocms.postgresqlservice.service.PostgresqlService;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +23,7 @@ public class PostgresqlController {
     @Autowired
     private GeneralService generalService;
 
-    @RequestMapping(value = "/postgresql/data/{module}/{templateCode}")
+    @RequestMapping(value = "/postgresql/data/{module}/{templateCode}", method = RequestMethod.POST)
     public Response getData(@PathVariable String module, @PathVariable String templateCode) {
         Response response = null;
         Document meta = new Document();
@@ -30,6 +32,69 @@ public class PostgresqlController {
         try {
             List<Document> dataDocument = postgresqlService.getData(module, templateCode);
             data.append("rdbms_data", dataDocument);
+            meta = generalService.getHttpStatusCode("200");
+        } catch (Exception e) {            
+            e.printStackTrace();
+            meta = generalService.getHttpStatusCode("400");
+            meta.put("stacktrace", e.getMessage());
+        }
+
+        response = new Response(meta, data);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/postgresql/insert/{module}/{templateCode}", method = RequestMethod.POST)
+    public Response insertData(@PathVariable String module, @PathVariable String templateCode, @RequestBody (required = false) String requestBody) {
+        Response response = null;
+        Document meta = new Document();
+        Document data = new Document();
+        
+        try {
+            requestBody = requestBody == null ? "{}" : requestBody;          
+            data = postgresqlService.insertData(module, templateCode, requestBody);
+            meta = generalService.getHttpStatusCode("200");
+        } catch (Exception e) {            
+            e.printStackTrace();
+            meta = generalService.getHttpStatusCode("400");
+            meta.put("stacktrace", e.getMessage());
+        }
+
+        response = new Response(meta, data);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/postgresql/update/{module}/{templateCode}", method = RequestMethod.POST)
+    public Response updateData(@PathVariable String module, @PathVariable String templateCode, @RequestBody (required = false) String requestBody) {
+        Response response = null;
+        Document meta = new Document();
+        Document data = new Document();
+        
+        try {
+            requestBody = requestBody == null ? "{}" : requestBody;          
+            data = postgresqlService.updateData(module, templateCode, requestBody);
+            meta = generalService.getHttpStatusCode("200");
+        } catch (Exception e) {            
+            e.printStackTrace();
+            meta = generalService.getHttpStatusCode("400");
+            meta.put("stacktrace", e.getMessage());
+        }
+
+        response = new Response(meta, data);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/postgresql/delete/{module}/{templateCode}", method = RequestMethod.POST)
+    public Response deleteData(@PathVariable String module, @PathVariable String templateCode, @RequestBody (required = false) String requestBody) {
+        Response response = null;
+        Document meta = new Document();
+        Document data = new Document();
+        
+        try {
+            requestBody = requestBody == null ? "{}" : requestBody;          
+            data = postgresqlService.deleteData(module, templateCode, requestBody);
             meta = generalService.getHttpStatusCode("200");
         } catch (Exception e) {            
             e.printStackTrace();
--- postgresql-service/src/main/java/com/indocms/postgresqlservice/service/PostgresqlService.java
+++ postgresql-service/src/main/java/com/indocms/postgresqlservice/service/PostgresqlService.java
@@ -2,6 +2,7 @@ package com.indocms.postgresqlservice.service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
@@ -41,17 +42,165 @@ public class PostgresqlService {
 
     public List<Document> getData(String module, String templateCode) throws Exception {
         List<Document> output= new ArrayList<>();
+        templateHeader = this.getTemplateHeader(module, templateCode);
+        String query = templateHeader.get("query_select").toString();
+        output = this.executeQuery(query);
+        return output;
+     }
 
+     public Document insertData(String module, String templateCode, String requestBody) throws Exception {
+        Document output = new Document();
+        if (requestBody == null || requestBody.equals("{}")) {
+            throw new Exception("Request body cannot be null or blank");
+        } else {
+            templateHeader = this.getTemplateHeader(module, templateCode);
+            Document requestBodyDoc = Document.parse(requestBody);   
+            System.out.println("this.convertReqBodyToInsertQuery(requestBodyDoc) : " + this.convertReqBodyToInsertQuery(requestBodyDoc));
+            String query = this.convertReqBodyToInsertQuery(requestBodyDoc);
+            Document executeUpdateDoc = this.executeUpdate(query);
+            int executeUpdateResult = executeUpdateDoc.getInteger("result");            
+            if (executeUpdateResult == 0) {
+                output.append("message", "Insert Failed");
+            } else {
+                output.append("message", "Insert Success");
+                if (executeUpdateDoc.get("generated_id") != null) {
+                    String executeUpdateGenId = executeUpdateDoc.get("generated_id").toString();
+                    String insertId = templateHeader.get("insert_id").toString();
+                    requestBodyDoc.append(insertId, executeUpdateGenId);
+                }                                 
+                output.append("insert_data", requestBodyDoc);
+            }
+            output.append("query", query);
+        }        
+        return output;
+     }
+
+     public Document updateData(String module, String templateCode, String requestBody) throws Exception {
+        Document output = new Document();
+        if (requestBody == null || requestBody.equals("{}")) {
+            throw new Exception("Request body cannot be null or blank");
+        } else {
+            templateHeader = this.getTemplateHeader(module, templateCode);
+            Document requestBodyDoc = Document.parse(requestBody);   
+            System.out.println("this.convertReqBodyToUpdateQuery(requestBodyDoc) : " + this.convertReqBodyToUpdateQuery(requestBodyDoc));
+            String query = this.convertReqBodyToUpdateQuery(requestBodyDoc);
+            Document executeUpdateDoc = this.executeUpdate(query);
+            System.out.println("executeUpdateDoc : " + executeUpdateDoc.toJson());
+            output.append("query", query);
+        }        
+        return output;
+     }
+
+     public Document deleteData(String module, String templateCode, String requestBody) throws Exception {
+        Document output = new Document();
+        if (requestBody == null || requestBody.equals("{}")) {
+            throw new Exception("Request body cannot be null or blank");
+        } else {
+            templateHeader = this.getTemplateHeader(module, templateCode);
+            Document requestBodyDoc = Document.parse(requestBody);   
+            System.out.println("this.convertReqBodyToDeleteQuery(requestBodyDoc) : " + this.convertReqBodyToDeleteQuery(requestBodyDoc));
+            String query = this.convertReqBodyToDeleteQuery(requestBodyDoc);
+            Document executeUpdateDoc = this.executeUpdate(query);
+            System.out.println("executeUpdateDoc : " + executeUpdateDoc.toJson());
+            output.append("query", query);
+        }        
+        return output;
+     }
+
+     public Document getTemplateHeader(String module, String templateCode) throws Exception {
+        Document output = null;
+        Document filter = new Document("module", module).append("template_code", templateCode);
+        Document projection = new Document();
+        List<Document> tmpOutput = mongoService.find(mainDatabase, templateHeaderCollection, filter, null, projection);
+        if (tmpOutput.size() > 0) {
+            output = tmpOutput.get(0);
+        } else {
+            throw new Exception("Template Code (" + templateCode + ") Not Found");
+        }
+        return output;
+    }
+
+    public String convertReqBodyToInsertQuery(Document requestBodyDoc) {
+        StringBuilder output = new StringBuilder("INSERT INTO ");
+        StringBuilder values = new StringBuilder("(");
+
+        String databaseName = templateHeader.get("database").toString();
+        String databaseTableSeparator = templateHeader.get("database_table_separator").toString();
+        String tableName = templateHeader.get("table").toString();
+        
+        output.append(databaseName).append(databaseTableSeparator).append(tableName);
+        output.append(" (");
+
+        int count = 0;
+        for (String key : requestBodyDoc.keySet()) {
+            Object keyValue = requestBodyDoc.get(key);
+            output.append("\"").append(key).append("\"");
+            values.append("'").append(keyValue).append("'");
+
+            if (count + 1 < requestBodyDoc.keySet().size()) {
+                output.append(", ");
+                values.append(", ");
+            }
+            count++;
+        }
+        values.append(")");        
+        output.append(") VALUES ").append(values);
+        return output.toString();
+    }
+
+    public String convertReqBodyToUpdateQuery(Document requestBodyDoc) {
+        StringBuilder output = new StringBuilder("UPDATE ");
+        String filterQuery = "";
+
+        String databaseName = templateHeader.get("database").toString();
+        String databaseTableSeparator = templateHeader.get("database_table_separator").toString();
+        String tableName = templateHeader.get("table").toString();
+        
+        output.append(databaseName).append(databaseTableSeparator).append(tableName);
+        output.append(" SET ");
+
+        String updateId = templateHeader.get("update_id").toString();
+        for (String key : requestBodyDoc.keySet()) {
+            Object keyValue = requestBodyDoc.get(key);
+            if (key.equals(updateId)) {
+                filterQuery = " WHERE \"" + updateId + "\" = '" + keyValue + "'";
+            } else {                
+                output.append("\"" + key + "\"").append(" = '").append(keyValue).append("', ");
+            }
+        }
+
+        String tmpQuery = output.toString();
+        output = new StringBuilder(tmpQuery.substring(0, tmpQuery.length() - 2));
+        output.append(filterQuery);
+        return output.toString();
+    }
+
+    public String convertReqBodyToDeleteQuery(Document requestBodyDoc) {
+        StringBuilder output = new StringBuilder("DELETE FROM ");
+
+        String databaseName = templateHeader.get("database").toString();
+        String databaseTableSeparator = templateHeader.get("database_table_separator").toString();
+        String tableName = templateHeader.get("table").toString();
+        
+        output.append(databaseName).append(databaseTableSeparator).append(tableName);
+
+        String deleteId = templateHeader.get("delete_id").toString();
+        String deleteIdValue = requestBodyDoc.get(deleteId).toString();
+        output.append(" WHERE \"").append(deleteId).append("\" = '").append(deleteIdValue).append("'");
+
+        return output.toString();
+    }
+
+    public List<Document> executeQuery(String query) throws Exception {
+        List<Document> output = new ArrayList<>();
         Connection connection = null;
         try {
-            templateHeader = this.getTemplateHeader(module, templateCode);
-
             Class.forName(postgresqlClass);
             connection = DriverManager.getConnection(postgresqlUri, postgresqlUsername, postgresqlPassword);
 
             Statement statement = connection.createStatement();
             // String query = "select * from \"INDO_CMS_SAMPLE_REPORT\"";
-            String query = templateHeader.get("query_select").toString();
+            // String query = templateHeader.get("query_select").toString();
             ResultSet resultSet = statement.executeQuery(query);
             ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
                         
@@ -72,19 +221,35 @@ public class PostgresqlService {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-        }        
+        }   
         return output;
-     }
+    }
 
-     public Document getTemplateHeader(String module, String templateCode) throws Exception {
-        Document output = null;
-        Document filter = new Document("module", module).append("template_code", templateCode);
-        Document projection = new Document();
-        List<Document> tmpOutput = mongoService.find(mainDatabase, templateHeaderCollection, filter, null, projection);
-        if (tmpOutput.size() > 0) {
-            output = tmpOutput.get(0);
-        } else {
-            throw new Exception("Template Code (" + templateCode + ") Not Found");
+    public Document executeUpdate(String query) throws Exception {
+        Document output = new Document();
+        Connection connection = null;
+        // int output = 0;
+        try {
+            Class.forName(postgresqlClass);
+            connection = DriverManager.getConnection(postgresqlUri, postgresqlUsername, postgresqlPassword);
+
+            // Statement statement = connection.createStatement();
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            int executeUpdateResult = statement.executeUpdate();
+            output.append("result", executeUpdateResult);
+            ResultSet resultSet = statement.getGeneratedKeys();
+            while (resultSet.next()) {
+                // System.out.println("resultSet" + resultSet.getObject(1));
+                output.append("generated_id", resultSet.getString(1));
+            }
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }                
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
         return output;
     }
