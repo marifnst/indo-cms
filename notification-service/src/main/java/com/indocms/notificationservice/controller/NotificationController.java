2c9ba423dfb7e075964742771cb0d504fc73ce0c
marifnst <arif.nasution@emeriocorp.com>
2019-08-25 22:38:51 +0700

    init notification-service with web socket

--- notification-service/src/main/java/com/indocms/notificationservice/controller/NotificationController.java
+++ notification-service/src/main/java/com/indocms/notificationservice/controller/NotificationController.java
@@ -5,10 +5,14 @@ import com.indocms.notificationservice.channel.NotificationChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @RestController
 @EnableBinding(NotificationChannel.class)
 public class NotificationController {
@@ -19,5 +23,12 @@ public class NotificationController {
     @RequestMapping(value="/notification/export", method = RequestMethod.GET)
     public void sendMessage() {
         notificationChannel.exportNotification().send(MessageBuilder.withPayload("Sample Export Notification").build());
+    }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/exportNotification")
+    public String greeting() throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return "Web Socket From Spring";
     }
 }
